bf2ffa6b5775f7bbf197f4ebcddcaa0f84fc2dda
Emilio Nuño Paz <superbob_99@hotmail.com>
2019-11-30 13:13:10 -0800

    Agregada vista de almacén

--- BDCucei.py
+++ BDCucei.py
@@ -9,9 +9,6 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-def presionar():
-    print("has dado click")
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -19,15 +16,58 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(350, 110, 55, 16))
+        self.pestanasBD = QtWidgets.QTabWidget(self.centralwidget)
+        self.pestanasBD.setGeometry(QtCore.QRect(0, 0, 800, 600))
+        self.pestanasBD.setMinimumSize(QtCore.QSize(800, 600))
+        self.pestanasBD.setObjectName("pestanasBD")
+        self.almacen = QtWidgets.QWidget()
+        self.almacen.setToolTip("")
+        self.almacen.setAccessibleName("")
+        self.almacen.setAccessibleDescription("")
+        self.almacen.setObjectName("almacen")
+        self.formLayoutWidget = QtWidgets.QWidget(self.almacen)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(170, 120, 401, 281))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+        self.label = QtWidgets.QLabel(self.formLayoutWidget)
         self.label.setObjectName("label")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(330, 280, 93, 28))
-        self.pushButton.setObjectName("pushButton")
-        
-        self.pushButton.clicked.connect(presionar)
-        
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label)
+        self.label_2 = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_2.setObjectName("label_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_2)
+        self.label_3 = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_3.setObjectName("label_3")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.LabelRole, self.label_3)
+        self.label_4 = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_4.setObjectName("label_4")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.LabelRole, self.label_4)
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.lineEdit_2)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.formLayout.setWidget(2, QtWidgets.QFormLayout.FieldRole, self.lineEdit_3)
+        self.lineEdit_4 = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.formLayout.setWidget(3, QtWidgets.QFormLayout.FieldRole, self.lineEdit_4)
+        self.btnAgregar = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.btnAgregar.setObjectName("btnAgregar")
+        self.formLayout.setWidget(4, QtWidgets.QFormLayout.FieldRole, self.btnAgregar)
+        self.btnMostrar = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.btnMostrar.setObjectName("btnMostrar")
+        self.formLayout.setWidget(5, QtWidgets.QFormLayout.FieldRole, self.btnMostrar)
+        self.btnEditar = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.btnEditar.setObjectName("btnEditar")
+        self.formLayout.setWidget(6, QtWidgets.QFormLayout.FieldRole, self.btnEditar)
+        self.btnEliminar = QtWidgets.QPushButton(self.formLayoutWidget)
+        self.btnEliminar.setObjectName("btnEliminar")
+        self.formLayout.setWidget(7, QtWidgets.QFormLayout.FieldRole, self.btnEliminar)
+        self.lineEdit = QtWidgets.QLineEdit(self.formLayoutWidget)
+        self.lineEdit.setObjectName("lineEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.lineEdit)
+        self.pestanasBD.addTab(self.almacen, "")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -38,13 +78,21 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.pestanasBD.setCurrentIndex(0)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Bienvenido"))
-        self.pushButton.setText(_translate("MainWindow", "Comenzar"))
+        self.label.setText(_translate("MainWindow", "idAlmacen"))
+        self.label_2.setText(_translate("MainWindow", "capacidad"))
+        self.label_3.setText(_translate("MainWindow", "numLlenos"))
+        self.label_4.setText(_translate("MainWindow", "numVacios"))
+        self.btnAgregar.setText(_translate("MainWindow", "Agregar"))
+        self.btnMostrar.setText(_translate("MainWindow", "Mostrar"))
+        self.btnEditar.setText(_translate("MainWindow", "Editar"))
+        self.btnEliminar.setText(_translate("MainWindow", "Eliminar"))
+        self.pestanasBD.setTabText(self.pestanasBD.indexOf(self.almacen), _translate("MainWindow", "Almacén"))
 
 
 if __name__ == "__main__":
